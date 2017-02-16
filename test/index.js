639f4207e8d52312a5e5eb27d3f03f44cf1ebdc7
Andres Rios <rianby64@gmail.com>
2017-02-17 01:50:16 +0300

    Test integrity of app - skeleton

--- test/index.js
+++ test/index.js
@@ -1,16 +1,19 @@
 'use strict';
 window.location.hash = "";
-promise_test(() => { return new Promise((resolve, reject) => {
+promise_test(function() { return new Promise(this.step_func((resolve, reject) => {
   document.querySelector('x-fragment').loaded.then((fragment) => {
     var counter = 0;
-    fragment.querySelector('[hash=""]').addEventListener('show', e => {
+    fragment.querySelector('[hash=""]').addEventListener('show', this.step_func(e => {
+      assert_true(fragment.querySelector('ul.tasks') instanceof HTMLElement);
       counter++;
       exit();
-    });
-    fragment.querySelector('[hash="add"]').addEventListener('show', e => {
+    }));
+    fragment.querySelector('[hash="add"]').addEventListener('show', this.step_func(e => {
       counter++;
+      assert_true(fragment.querySelector('input[name="description"]') instanceof HTMLElement);
+      assert_true(fragment.querySelector('[action="add"]') instanceof HTMLElement);
       exit();
-    });
+    }));
 
     function exit() {
       if (counter === 2) {
@@ -21,5 +24,15 @@ promise_test(() => { return new Promise((resolve, reject) => {
     window.location.hash = "add";
     window.location.hash = "";
   });
+})); }, "Routers are present");
 
-}); }, "Routers are present");
+promise_test(function() { return new Promise((resolve, reject) => {
+  document.querySelector('x-fragment').loaded.then(this.step_func((fragment) => {
+    fragment.querySelector('[hash="add"]').addEventListener('show', this.step_func(e => {
+
+      resolve();
+    }));
+
+    window.location.hash = "add";
+  }));
+}); }, "Add a new task");
