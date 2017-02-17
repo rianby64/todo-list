f316d2d3c6230bea26f8de978c9713dbe52383cf
Andres Rios <rianby64@gmail.com>
2017-02-17 11:45:27 +0300

    A lot of changes...

--- src/app.js
+++ src/app.js
@@ -1,22 +1,33 @@
-'use strict';
+(() => {
+  'use strict';
 
-document.querySelector('[hash=""]').addEventListener('show', e => {
-  var frame = e.detail.router;
-});
+  document.querySelector('[hash=""]').addEventListener('show', e => {
+    var frame = e.detail.router;
+    return frame;
+  });
 
-document.querySelector('[hash="add"]').addEventListener('show', e => {
-  var frame = e.detail.router;
-});
+  document.querySelector('[hash="add"]').addEventListener('show', e => {
+    var frame = e.detail.router;
+    var input = frame.querySelector('input');
+    if (input) {
+    }
+    return frame;
+  });
 
-document.querySelector('[action="add"]').addEventListener('click', e => {
-  var frame = document.querySelector('[hash="add"]');
-  var list = document.querySelector('ul.tasks');
-  var input = document.querySelector('input[name="description"]');
+  document.querySelector('[action="add"]').addEventListener('click', () => {
+    var frame = document.querySelector('[hash="add"]');
+    var list = document.querySelector('ul.tasks');
+    var input = document.querySelector('input[name="description"]');
 
-  var li = document.importNode(frame.querySelector('template').content, true);
-  var id = li.querySelector('[name="id"]');
-  var description = li.querySelector('[name="description"]');
-  description.textContent = input.value;
-  list.appendChild(li);
-  id.textContent = list.children.length;
-});
+    var li = document.importNode(frame.querySelector('template').content, true);
+    var id = li.querySelector('[name="id"]');
+    var edit = li.querySelector('[action="edit"]');
+    var description = li.querySelector('[name="description"]');
+    description.textContent = input.value;
+    list.appendChild(li);
+    id.textContent = list.children.length;
+
+    edit.href = `#add/${id.textContent}`;
+    input.value = '';
+  });
+})();
--- test/index.js
+++ test/index.js
@@ -3,17 +3,21 @@ window.location.hash = "";
 promise_test(function() { return new Promise(this.step_func((resolve, reject) => {
   document.querySelector('x-fragment').loaded.then((fragment) => {
     var counter = 0;
-    fragment.querySelector('[hash=""]').addEventListener('show', this.step_func(e => {
+    var handler1 = this.step_func(e => {
       assert_true(fragment.querySelector('ul.tasks') instanceof HTMLElement);
       counter++;
+      fragment.querySelector('[hash=""]').removeEventListener('show', handler1);
       exit();
-    }));
-    fragment.querySelector('[hash="add"]').addEventListener('show', this.step_func(e => {
+    });
+    var handler2 = this.step_func(e => {
       counter++;
       assert_true(fragment.querySelector('input[name="description"]') instanceof HTMLElement);
       assert_true(fragment.querySelector('[action="add"]') instanceof HTMLElement);
+      fragment.querySelector('[hash="add"]').removeEventListener('show', handler2);
       exit();
-    }));
+    });
+    fragment.querySelector('[hash=""]').addEventListener('show', handler1);
+    fragment.querySelector('[hash="add"]').addEventListener('show', handler2);
 
     function exit() {
       if (counter === 2) {
@@ -29,17 +33,21 @@ promise_test(function() { return new Promise(this.step_func((resolve, reject) =>
 promise_test(function() { return new Promise((resolve, reject) => {
   document.querySelector('x-fragment').loaded.then(this.step_func((fragment) => {
     var input = document.querySelector('input[name="description"]');
-    input.value = "Task 1";
+    var text = "Task 1";
+    input.value = text;
     var btnadd = document.querySelector('[action="add"]');
-    btnadd.addEventListener('click', this.step_func(e => {
+    var handler = this.step_func(e => {
       var list = document.querySelector('ul.tasks');
       var task = list.lastElementChild;
       assert_true(task instanceof HTMLLIElement);
-      assert_equals(task.querySelector('[name="description"]').textContent, input.value);
+      assert_equals(task.querySelector('[name="description"]').textContent, text);
       assert_equals(task.querySelector('[name="id"]').textContent, list.querySelectorAll('li').length.toString());
+      assert_equals(input.value, "");
+      btnadd.removeEventListener('click', handler);
       resolve();
-    }));
-    btnadd.dispatchEvent(new Event('click'));
+    });
+    btnadd.addEventListener('click', handler);
+    btnadd.dispatchEvent(new MouseEvent('click'));
   }));
 }); }, "Add a new task");
 
@@ -47,7 +55,15 @@ promise_test(function() { return new Promise((resolve, reject) => {
   document.querySelector('x-fragment').loaded.then(this.step_func((fragment) => {
     var list = document.querySelector('ul.tasks');
     var task = list.lastElementChild;
-    assert_true(task.querySelector('[action="edit"]') instanceof HTMLElement);
-    resolve();
+    var edit = task.querySelector('[action="edit"]');
+    assert_true(edit instanceof HTMLElement);
+    var editRoute = document.querySelector('[hash="add"] [hash="([0-9]+)"]');
+    var handler = this.step_func(e => {
+      editRoute.removeEventListener('show', handler);
+      assert_equals(e.detail.param1, task.querySelector('[name="id"]').textContent);
+      resolve();
+    });
+    editRoute.addEventListener('show', handler);
+    edit.dispatchEvent(new MouseEvent('click'));
   }));
 }); }, "Edit last added task");
