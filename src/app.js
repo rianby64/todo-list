440beacb5f3431bc13c2c68b3943b3fc1c5e5780
Andres Rios <rianby64@gmail.com>
2017-02-17 13:47:43 +0300

    Add anchors and retabulate code

--- src/app.js
+++ src/app.js
@@ -4,32 +4,34 @@
   document.querySelector('[hash="tasks"]').addEventListener('show', () => {
   });
 
-  document.querySelector('[hash="task"] [hash="new"]').addEventListener('show', () => {
-    var input = document.querySelector('input[name="description"]');
-    input.value = '';
-  });
+  document.querySelector('[hash="task"] [hash="new"]')
+    .addEventListener('show', () => {
+      var input = document.querySelector('input[name="description"]');
+      input.value = '';
+    });
 
-  document.querySelector('[action="add"]').addEventListener('click', () => {
-    var frame = document.querySelector('[hash="task"]');
-    var list = document.querySelector('ul.tasks');
-    var input = document.querySelector('input[name="description"]');
+  document.querySelector('[action="add"]')
+    .addEventListener('click', () => {
+      var frame = document.querySelector('[hash="task"]');
+      var list = document.querySelector('ul.tasks');
+      var input = document.querySelector('input[name="description"]');
 
-    var tmpl = document.importNode(frame.querySelector('template').content, true);
+      var tmpl = document.importNode(frame.querySelector('template').content, true);
 
-    var li = tmpl.querySelector('li');
-    var id = tmpl.querySelector('[name="id"]');
-    var edit = tmpl.querySelector('[action="edit"]');
-    var description = tmpl.querySelector('[name="description"]');
+      var li = tmpl.querySelector('li');
+      var id = tmpl.querySelector('[name="id"]');
+      var edit = tmpl.querySelector('[action="edit"]');
+      var description = tmpl.querySelector('[name="description"]');
 
-    list.appendChild(tmpl);
+      list.appendChild(tmpl);
 
-    description.textContent = input.value;
-    id.textContent = list.children.length;
-    li.id = 'task-' + id.textContent;
-    edit.href = `#task/${id.textContent}`;
+      description.textContent = input.value;
+      id.textContent = list.children.length;
+      li.id = 'task-' + id.textContent;
+      edit.href = `#task/${id.textContent}`;
 
-    input.value = '';
-  });
+      input.value = '';
+    });
 
   document.querySelector('[hash="task"] [hash="([0-9]+)"]')
     .addEventListener('show', e => {
@@ -51,4 +53,5 @@
 
       description.textContent = input.value;
     });
+
 })();
