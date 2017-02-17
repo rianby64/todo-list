53bebf94a6c91e17e2f45a81d8e69b8ff099d018
Andres Rios <rianby64@gmail.com>
2017-02-17 16:01:38 +0300

    Test filter 'done'

--- test/index.js
+++ test/index.js
@@ -148,6 +148,23 @@ promise_test(function() { return new Promise((resolve, reject) => {
 promise_test(function() { return new Promise((resolve, reject) => {
   document.querySelector('x-fragment').loaded.then(this.step_func((fragment) => {
 
-    resolve();
+    var byDone = document.querySelector('a[filter="done"]');
+    var listRouter = document.querySelector('[hash="tasks"] [hash="([a-z]+)"]');
+
+    this.step_timeout(() => {
+      var handlerDone = this.step_func((e) => {
+        var router = e.detail.router;
+        router.removeEventListener('show', handlerDone);
+
+        assert_false(router.querySelector('#task-1').hidden);
+        assert_true(router.querySelector('#task-2').hidden);
+        assert_false(router.querySelector('#task-3').hidden);
+        assert_true(router.querySelector('#task-4').hidden);
+
+        resolve();
+      });
+      listRouter.addEventListener('show', handlerDone);
+      byDone.dispatchEvent(new MouseEvent('click'));
+    });
   }));
 }); }, "Filter by 'done'");
