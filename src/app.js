4b1141685220cd5b056ab411c7e60550ef5f7fd3
Andres Rios <rianby64@gmail.com>
2017-02-17 16:30:42 +0300

    Add small fixes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 (() => {
   'use strict';
 
-  document.querySelector('[hash="tasks"] [hash="([a-z]+)"]').addEventListener('show', (e) => {
-    var filter = e.detail.param1;
-    var frame = e.detail.router;
-    var tasks = [].slice.call(frame.querySelectorAll('li'));
-    tasks.forEach(task => {
-      task.hidden = false;
-      if (filter === 'all') { return; }
-      if (task.getAttribute('status') !== filter) {
-        task.hidden = true;
-      }
+  document.querySelector('[hash="tasks"] [hash="([a-z]+)"]')
+    .addEventListener('show', (e) => {
+      var filter = e.detail.param1;
+      var frame = e.detail.router;
+      var tasks = [].slice.call(frame.querySelectorAll('li'));
+      tasks.forEach(task => {
+        task.hidden = false;
+        if (filter === 'all') { return; }
+        if (task.getAttribute('status') !== filter) {
+          task.hidden = true;
+        }
+      });
     });
-  });
 
   document.querySelector('[hash="task"] [hash="new"]')
     .addEventListener('show', () => {
@@ -41,12 +42,14 @@
       li.setAttribute('status', 'active');
       edit.href = `#task/${id.textContent}`;
 
-      li.querySelector('[status="done"]').addEventListener('click', e => {
-        li.setAttribute('status', 'done');
+      li.querySelector('[status="done"]')
+      .addEventListener('click', e => {
+        e.target.closest('li').setAttribute('status', 'done');
       });
 
-      li.querySelector('[status="active"]').addEventListener('click', e => {
-        li.setAttribute('status', 'active');
+      li.querySelector('[status="active"]')
+      .addEventListener('click', e => {
+        e.target.closest('li').setAttribute('status', 'active');
       });
 
       input.value = '';
